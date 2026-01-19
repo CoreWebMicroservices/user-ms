d89d094356e26cac76f441966dedeccb7919b36a
Dima <dmi.mishchenko@gmail.com>
2026-01-19 18:01:18 +0100

    Excluded common Security from UserMs

--- user-service/src/main/java/com/corems/userms/app/UserServiceApplication.java
+++ user-service/src/main/java/com/corems/userms/app/UserServiceApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 
 import com.corems.common.security.config.CoremsJwtProperties;
+import com.corems.common.security.config.CoreMsPermitAllSecurityConfig;
+import com.corems.common.security.config.CoreMsSecurityConfig;
 import com.corems.common.security.service.TokenProvider;
 
 @EnableConfigurationProperties(CoremsJwtProperties.class)
-@SpringBootApplication
+@SpringBootApplication(exclude = {CoreMsSecurityConfig.class, CoreMsPermitAllSecurityConfig.class})
 public class UserServiceApplication {
 
     @Bean
